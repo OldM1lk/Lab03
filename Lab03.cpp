f4341d58bfb34f05082fa8ecd20f8cccf969b6db
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2023-10-06 13:12:22 +0700

    Create Lab03.cpp

---
+++ Lab03.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  int countT;
+  double fi, E, C, T0, TP, trash;
+  cout << "Введите E = ";
+  cin >> E;
+  cout << "Введите C = ";
+  cin >> C;
+  cout << "Введите T0 = ";
+  cin >> T0;
+  cout << "Введите TP = ";
+  cin >> TP;
+  cout << "Введите кол-во T = ";
+  cin >> countT;
+  cout.precision(3);
+  cout << fixed;
+  
+  double T[countT];
+  
+  for (int i = 0; i < countT; ++i) {
+    cout << "Введите " << i + 1 << " элемент T = ";
+    cin >> T[i];
+  }
+  
+  for (int j = 0; j < countT; ++j) {
+    fi = (T[j] - TP) / (T0 - TP);
+    if (fi < 0.4) {
+      trash = 1.9 * pow( (T[j] / T0), 1.7 ) * C;
+    } else if (fi > 0.4) {
+      trash = 525 * exp( -(E / (4.575 * T[j]) ) ) * pow(C, 2);
+    }
+    cout << "При T = " << T[j] << " fi = " << fi << " trash = " << trash << endl;
+  }
+}
